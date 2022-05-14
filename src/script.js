6dde0f4af1a6e4e1adb0f3619dbdaa2921d763bf
Anon-136 <anonjin44@gmail.com>
2022-05-14 15:56:48 +0700

    Add example terrain script

--- src/script.js
+++ src/script.js
@@ -1,7 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { ImprovedNoise } from 'three/examples/jsm/math/ImprovedNoise.js'
 import * as dat from 'dat.gui'
 
 // Debug
