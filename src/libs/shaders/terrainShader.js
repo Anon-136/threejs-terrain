dc3d6b1d714d4224f37942229d316154934b7377
Sahatsarin Pawanna <58083752+Ricecamera@users.noreply.github.com>
2022-05-24 15:20:40 +0700

    Update shader to change color with different height

--- src/libs/shaders/terrainShader.js
+++ src/libs/shaders/terrainShader.js
@@ -6,16 +6,30 @@ export const terrainShader = (function () {
 // Outputs
 varying vec3 vNormal;
 varying vec3 vPosition;
-
+varying vec3 vColor;
 
 #define saturate(a) clamp( a, 0.0, 1.0 )
 
-void main(){
-  gl_Position = projectionMatrix * modelViewMatrix * vec4(position.x, position.y, position.z, 1.0);
-
+void main() {
   vNormal = normal;
-
   vPosition = position.xyz;
+
+  float heightValue = clamp(position.y / 255.0, 0.0, 1.0);
+
+  if (heightValue < 0.2) {
+    vColor = vec3(0.0, 0.0, 1.0);
+  }
+  else if (heightValue < 0.5) {
+    vColor = vec3(0.0588, 0.6118, 0.4);
+  }
+  else if (heightValue < 0.8) {
+    vColor = vec3(0.4784, 0.3647, 0.1451);
+  }
+  else {
+    vColor = vec3(1.0, 1.0, 1.0);
+  }
+
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(position.x, position.y, position.z, 1.0);
 } 
 `
 
@@ -24,18 +38,17 @@ void main(){
 // Inputs from vertextShader
 varying vec3 vNormal;
 varying vec3 vPosition;
+varying vec3 vColor;
+
 
 void main() {
   vec3 light = vec3(1, 1, 1);
   light = normalize(light);
-
-  float heightValue = clamp(vPosition.y / 255.0, 0.0, 1.0);
-
   float dProd = max(0.0, dot(vNormal, light));
 
-  float color = dProd * heightValue;
+  vec3 color = dProd * vColor;
 
-  gl_FragColor = vec4(color, 0.0, 0.0, 1.0);
+  gl_FragColor = vec4(color, 1.0);
 }
 `
 
