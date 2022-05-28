4fea3a7dd3d34d808a0bde42c628d099981decd0
Sahatsarin Pawanna <58083752+Ricecamera@users.noreply.github.com>
2022-05-28 12:31:15 +0700

    Add intersect and differ operation fo js object

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -10,3 +10,21 @@ export function randomRangeInt(min, max) {
   max = Math.floor(max)
   return Math.floor(Math.random() * (max - min + 1) + min) //The maximum is inclusive and the minimum is inclusive
 }
+
+export function dictIntersection(dictA, dictB) {
+  const intersection = {}
+  for (let k in dictB) {
+    if (k in dictA) {
+      intersection[k] = dictA[k]
+    }
+  }
+  return intersection
+}
+
+export function dictDifference(dictA, dictB) {
+  const diff = { ...dictA }
+  for (let k in dictB) {
+    delete diff[k]
+  }
+  return diff
+}
